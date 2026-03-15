922fe0669042f7c6717c8f9a9a28162f073d9298
tungsten <goat69baa@gmail.com>
2026-03-15 06:48:01 +0200

    Update ba ba revoloution.py

--- ba ba revoloution.py
+++ ba ba revoloution.py
@@ -26,4 +26,4 @@ new_string = "".join(map(parseplus, text))
 with open("out.txt", "w") as f:
     f.write(f"=t tile:5/3&arrow:u:rot-45:5/4 tile:green&arrow:u:lime tile:1/2&arrow:u:rot45:1/3\ntile:gold&arrow:l:yellow tile&baba:[!/{new_string}] tile:4/3&arrow:blue\ntile:brown&arrow:d:rot45:orange tile:maroon&arrow:d:red tile:3/0&arrow:d:rot-45:purple\n")
 print(f"=t tile:5/3&arrow:u:rot-45:5/4 tile:green&arrow:u:lime tile:1/2&arrow:u:rot45:1/3\ntile:gold&arrow:l:yellow tile&baba:[!/{new_string}] tile:4/3&arrow:blue\ntile:brown&arrow:d:rot45:orange tile:maroon&arrow:d:red tile:3/0&arrow:d:rot-45:purple\n")
-#q is up left, e is up right, z is down left, c is down right
+#q is up left, e is up right, z is down left, c is down right. u is up, d is down, r is right and l is left
